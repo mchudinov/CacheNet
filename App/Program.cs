ed3616ee16fbc05595e202d386157eae8ec3aac6
Mikael Chudinov <mikael@chudinov.net>
2017-02-20 14:08:09 +0100

    Add sync and async extensions.

--- App/Program.cs
+++ App/Program.cs
@@ -1,24 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
+using CacheCSharp;
 
 namespace App
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Test memory cache");
+            Console.WriteLine(">> Test memory cache");
             CacheVB.ICache cacheManagerVB = new CacheVB.MemoryProvider();
             cacheManagerVB.Set("key1", new Widget { Name = "memory cache VB", Number = 1 });
             var temp = cacheManagerVB.Get<Widget>("key1");
-            Console.WriteLine(temp.Number + "-" + temp.Name);
+            Console.WriteLine(temp.Number + "-" + temp.Name);            
 
+            CacheCSharp.ICache cacheCacheManagerCScacheMemoryCS = new CacheCSharp.MemoryProvider();
             CacheCSharp.ICache cacheMemoryCS = new CacheCSharp.MemoryProvider();
             cacheMemoryCS.Set("key2", new Widget {Name = "memory cache C#", Number = 2});
             temp = cacheMemoryCS.Get<Widget>("key2");
             Console.WriteLine(temp.Number + "-" + temp.Name);
 
 
-            Console.WriteLine("Test System.Web cache");
+            Console.WriteLine(">> Test System.Web cache");
             CacheVB.ICache cacheSystemWebVB = new CacheVB.SystemWebProvider();
             cacheSystemWebVB.Set("key3", new Widget { Name = "System.Web cache VB", Number = 3 });
             temp = cacheSystemWebVB.Get<Widget>("key3");
@@ -30,7 +33,7 @@ namespace App
             temp = cacheSystemWebCS.Get<Widget>("key4");
             Console.WriteLine(temp.Number + "-" + temp.Name);
 
-            Console.WriteLine("Test CacheManager cache");
+            Console.WriteLine(">> Test CacheManager cache");
             CacheVB.ICache cacheCacheManagerVB = new CacheVB.CacheManagerProvider();
             cacheCacheManagerVB.Set("key5", new Widget { Name = "CacheManager cache VB", Number = 5 });
             temp = cacheCacheManagerVB.Get<Widget>("key5");
@@ -42,6 +45,14 @@ namespace App
             temp = cacheCacheManagerCS.Get<Widget>("key6");
             Console.WriteLine(temp.Number + "-" + temp.Name);
 
+            Console.WriteLine(">> Extension methods");
+            Console.WriteLine(cacheCacheManagerCS.UseCached<string>("key7", temp.GetValue, 10));
+            
+
+            var asyncResult = cacheCacheManagerCS.UseCachedAsync<string>("key8", temp.GetValueAsync, 10);
+            Task.WaitAll(asyncResult); // block while the task completes
+            Console.WriteLine(asyncResult.Result);
+
             Console.ReadLine();
         }
 
@@ -49,6 +60,17 @@ namespace App
         {
             public int Number { get; set; }
             public string Name { get; set; }
+
+            public string GetValue()
+            {
+                return "sync value return";
+            }
+
+            public async Task<string> GetValueAsync()
+            {
+                await Task.Delay(1000);
+                return "async method return";
+            }
         }
     }
 }
--- CacheCSharp/CacheProviderExtensions.cs
+++ CacheCSharp/CacheProviderExtensions.cs
@@ -5,7 +5,7 @@ namespace CacheCSharp
 {
     public static class CacheProviderExtensions
     {
-        public static async Task<T> UseCached<T>(this ICache cache, string key, Func<Task<T>> factory, int? duration = null) where T: class
+        public static async Task<T> UseCachedAsync<T>(this ICache cache, string key, Func<Task<T>> factory, int? duration = null) where T: class
         {
             var cached = cache.Get<T>(key);
             if (cached != null)
@@ -14,6 +14,27 @@ namespace CacheCSharp
             }
 
             var value = await factory();
+            if (duration.HasValue)
+            {
+                cache.Set(key, value, duration.Value);
+            }
+            else
+            {
+                cache.Set(key, value);
+            }
+
+            return value;
+        }
+
+        public static T UseCached<T>(this ICache cache, string key, Func<T> factory, int? duration = null) where T : class
+        {
+            var cached = cache.Get<T>(key);
+            if (cached != null)
+            {
+                return cached;
+            }
+
+            var value = factory();
             if (duration.HasValue)
             {
                 cache.Set(key, value, duration.Value);
